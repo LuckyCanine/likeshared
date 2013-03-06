f01010376879d4e075816107ca167b2c1b201eac
TKS <tks@tks-Aspire-X5900.(none)>
2013-03-06 01:06:12 -0800

    friends

--- exampleapp.py
+++ exampleapp.py
@@ -197,7 +197,7 @@ def index():
 	#for like in likes:
 	#	friendslikes.append(like)
 	
-	listlen = len(friendslikes.data)
+	listlen = len(friendslikes)
         app_friends = fql(
             "SELECT uid, name, is_app_user, pic_square "
             "FROM user "
